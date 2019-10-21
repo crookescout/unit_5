2ca87510218a747fea17a7886263e55885b74046
scout <scout.crooke@ssfs.org>
2019-10-21 14:14:49 -0400

    Worked on Unit 5 assignment option 1

--- assignment_option_1.py
+++ assignment_option_1.py
@@ -1,2 +1,25 @@
 # Scout Crooke, 10/21/19, This program runs a guessing game
 
+
+import random
+
+while True:
+    question = input("Do you want to play this guessing game? (yes or no)")
+    if question == "no":
+        print("Alright, see you later!")
+        break
+    elif question == "yes":
+        num = random.randint(1, 100)
+        print("Great lets play! Guess a number between 1 and 100")
+        guess = int(input("What is your number guess?"))
+        if guess > num:
+            print("Your number is to high")
+            guess = int(input("Guess again!"))
+        elif guess < num:
+            print("Your number is too low")
+            guess = int(input("Guess again!"))
+        elif guess == num:
+            print("Awesome! You guessed my number?")
+        break
+
+
--- u5_daily_exercises.py
+++ u5_daily_exercises.py
@@ -1,64 +1,64 @@
 # Scout Crooke, 10/17/19, this program works on unit 5 daily exercises
 
 import random
-#
-# for x in range(21):
-#     print(x)
-#
-#
-# n = int(input("What is the upper value?"))
-# for y in range(1, n + 1):
-#     print(y)
-#
-#
-# print("Be careful, the upper value can not be smaller than the lower value!")
-# m = int(input("What is the lower value?"))
-# n = int(input("What is the upper value?"))
-# for y in range(m, n + 1):
-#     print(y)
-#
-#
-# a = int(input("What is the lower value?"))
-# b = int(input("What is the upper value?"))
-# for z in range(a, b - 1, -1):
-#     print(z)
-#
-# print("Here are the numbers from 10 to 20:")
-# for x in range(10, 21):
-#     print(x)
-#
-# print("Here are the even numbers from 2 to 40:")
-# for x in range(2, 41, 2):
-#     print(x)
-#
-# n = int(input("What is the positive integer you want the multiplication table for?"))
-# for x in range(13):
-#     print(n, "*", x, "=", x * n)
 
-# even_sum = 0
-# odd_sum = 0
-# for x in range(10):
-#     num = random.randint(1, 100)
-#     if (num % 2) == 0:
-#         even_sum = even_sum + num
-#     else:
-#         odd_sum = odd_sum + num
-#
-# print(even_sum)
-# print(odd_sum)
+for x in range(21):
+    print(x)
 
-# num_sum = 0
-# for x in range(1000):
-#     if (x % 3) == 0 or (x % 5) == 0:
-#         num_sum = num_sum + x
-#
-# print(num_sum)
 
-# x = 0
-# num = int(input("Give me a number please"))
-# while x < num + 1:
-#     print("* " * x)
-#     x = x + 1
+n = int(input("What is the upper value?"))
+for y in range(1, n + 1):
+    print(y)
+
+
+print("Be careful, the upper value can not be smaller than the lower value!")
+m = int(input("What is the lower value?"))
+n = int(input("What is the upper value?"))
+for y in range(m, n + 1):
+    print(y)
+
+
+a = int(input("What is the lower value?"))
+b = int(input("What is the upper value?"))
+for z in range(a, b - 1, -1):
+    print(z)
+
+print("Here are the numbers from 10 to 20:")
+for x in range(10, 21):
+    print(x)
+
+print("Here are the even numbers from 2 to 40:")
+for x in range(2, 41, 2):
+    print(x)
+
+n = int(input("What is the positive integer you want the multiplication table for?"))
+for x in range(13):
+    print(n, "*", x, "=", x * n)
+
+even_sum = 0
+odd_sum = 0
+for x in range(10):
+    num = random.randint(1, 100)
+    if (num % 2) == 0:
+        even_sum = even_sum + num
+    else:
+        odd_sum = odd_sum + num
+
+print(even_sum)
+print(odd_sum)
+
+num_sum = 0
+for x in range(1000):
+    if (x % 3) == 0 or (x % 5) == 0:
+        num_sum = num_sum + x
+
+print(num_sum)
+
+x = 0
+num = int(input("Give me a number please"))
+while x < num + 1:
+    print("* " * x)
+    x = x + 1
 
 while True:
     num = input("What number do you want to use?")
